95fb77b5445d15dd3fa1eca116ab80c131d5d752
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2024-01-08 17:07:27 -0300

    feat: finished

--- src/main/java/com/remedios/matheus/curso/infra/security/SecurityFilter.java
+++ src/main/java/com/remedios/matheus/curso/infra/security/SecurityFilter.java
@@ -1,9 +1,14 @@
 package com.remedios.matheus.curso.infra.security;
 
+import com.remedios.matheus.curso.domain.usuario.repository.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,20 +16,40 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = recuperarToken(request);
+        if (token != null && !token.isEmpty()) {
+            try {
+                String subject = tokenService.getSubject(token);
+                UserDetails byLogin = usuarioRepository.findByLogin(subject);
+
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(byLogin, null, byLogin.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } catch (RuntimeException e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Token inválido ou expirado.");
+                return;
+            }
+        }
 
         filterChain.doFilter(request, response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         String authorization = request.getHeader("Authorization");
-        if (authorization == null) {
-            throw new RuntimeException("Token não enviado.");
+        System.out.println(authorization);
+
+        if (authorization != null) {
+            return authorization.replace("Bearer ", "");
         }
 
-        return authorization;
+        return null;
     }
 }
--- src/main/java/com/remedios/matheus/curso/infra/security/TokenService.java
+++ src/main/java/com/remedios/matheus/curso/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.remedios.matheus.curso.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.remedios.matheus.curso.domain.usuario.entity.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -20,14 +21,30 @@ public class TokenService {
     public String gerarToken(Usuario usuario) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-
             return JWT.create()
                     .withIssuer("remedios_api")
                     .withSubject(usuario.getLogin())
                     .withExpiresAt(dataExpiracao())
                     .sign(algorithm);
+
         } catch (JWTCreationException e) {
-            throw new RuntimeException("Erro ao gerar token", e);
+            throw new RuntimeException("Erro ao gerar token");
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String subject = JWT.require(algorithm)
+                    .withSubject("remedios_api")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+            System.out.println(subject);
+            return subject;
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException("Token inválido ou expirado.", e);
         }
     }
 
